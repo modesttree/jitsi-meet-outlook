28af5a01578a1d1b34bd54d3056ead46596a94f0
amoussa <amoussa@modesttree.com>
2021-05-21 15:39:36 -0300

    Added ability to create meeting from new meeting tab (fixes T10272)

--- LeafOutlook/FormSettings.language.cs
+++ LeafOutlook/FormSettings.language.cs
@@ -27,6 +27,7 @@ namespace LeafOutlook
             this.labelRequireDisplayName.Text = Globals.ThisAddIn.getElementTranslation("settings", "labelRequireDisplayName");
             this.labelStartWithAudioMuted.Text = Globals.ThisAddIn.getElementTranslation("settings", "labelStartWithAudioMuted");
             this.labelStartWithVideoMuted.Text = Globals.ThisAddIn.getElementTranslation("settings", "labelStartWithVideoMuted");
+            this.labelRibbonOnEveryAppointment.Text = Globals.ThisAddIn.getElementTranslation("settings", "labelRibbonOnEveryAppointment");
             this.groupBoxLanguage.Text = Globals.ThisAddIn.getElementTranslation("settings", "groupBoxLanguage");
 
         }
